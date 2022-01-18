a29612ea30f0785dda319647fcaaf2bdd7168378
Arthur Picard <arthur.picard7@gmail.com>
2022-01-18 13:34:22 +0100

    Update: Render Tasks in App

--- imports/ui/components/Task.jsx
+++ imports/ui/components/Task.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import './Task.scss';
 
-const Task = ({ task }) => {
+export const Task = ({ task }) => {
   return (
     <div>
-      <h1>Create a task</h1>
       <li>{task.text}</li>
     </div>
   )
