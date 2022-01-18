fc663d42f9b9daae68b395457aebcecf1df92dff
Arthur Picard <arthur.picard7@gmail.com>
2022-01-18 16:54:53 +0100

    Update: Server Startup, need login to check to do list

--- server/main.js
+++ server/main.js
@@ -2,15 +2,16 @@ import { Meteor } from 'meteor/meteor';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { Accounts } from 'meteor/accounts-base';
 
+const insertTask = (taskText, user) =>
+  TasksCollection.insert({
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(),
+  });
+
 // Creating User account:
 const SEED_USERNAME = 'meteorite';
 const SEED_PASSWORD = 'password';
-
-function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
-}
-
-const insertTask = taskText => TasksCollection.insert({ text: taskText });
 
 Meteor.startup(() => {
   if (!Accounts.findUserByUsername(SEED_USERNAME)) {
@@ -19,7 +20,9 @@ Meteor.startup(() => {
       password: SEED_PASSWORD,
     });
   }
-  
+
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
+
   if (TasksCollection.find().count() === 0) {
     [
       'First Task',
@@ -29,6 +32,6 @@ Meteor.startup(() => {
       'Fifth Task',
       'Sixth Task',
       'Seventh Task'
-    ].forEach(insertTask)
+    ].forEach(taskText => insertTask(taskText, user));
   }
 });
